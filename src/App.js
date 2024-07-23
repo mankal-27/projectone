c9033eb9103c61bc454bb829f321568ca821fe35
mankal-27 <mnkalkutagi@gmail.com>
2024-07-23 18:55:30 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import ContactCards from "./ContactCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    // const contacts = [
+    //     {
+    //         avatarUrl:"https://placehold.co/150",
+    //         name: "Harry Potter",
+    //         email:"Hp@hp.com",
+    //         age: 34
+    //     },
+    //     {
+    //         avatarUrl:"https://placehold.co/150",
+    //         name: "Hormonie Gringer",
+    //         email:"Hrmging@hp.com",
+    //         age: 31
+    //     },
+    //     {
+    //         avatarUrl:"https://placehold.co/150",
+    //         name: "Neviel longbottom",
+    //         email:"nvlbtm@hp.com",
+    //         age: 28
+    //     },
+    //     {
+    //         avatarUrl:"https://placehold.co/150",
+    //         name: "Severus Snape",
+    //         email:"sevsnape@hp.com",
+    //         age: 45
+    //     }
+    // ]
+
+    const [results,setResults] = useState([]);
+    
+    useEffect(() => {
+        fetch("https://randomuser.me/api/?results=5")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setResults(data.results)
+        })
+    }, [])
+    
+    return(
+        <>
+        {results.map((result, index) => {
+            return(
+            <ContactCards key={index}
+                avatarUrl = {result.picture.large}
+                name = {result.name.first} 
+                email= {result.email}
+                age= {result.dob.age}
+            />)
+        })}
+        </>
+    )
 }
 
 export default App;
